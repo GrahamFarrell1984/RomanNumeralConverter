2433f1b9fe7cd15e4d8f0de688e330d8063ba96a
Graham Farrell <grahamfarrell1984@gmail.com>
2019-10-28 10:57:27 +0000

    Added decimal number and Roman numeral arrays to RomanNumeralConverter class.

--- src/main/java/com/mycompany/romannumeralconverter/RomanNumeralConverter.java
+++ src/main/java/com/mycompany/romannumeralconverter/RomanNumeralConverter.java
@@ -11,4 +11,7 @@ package com.mycompany.romannumeralconverter;
  */
 public class RomanNumeralConverter {
     
+    public final int[] DECIMAL_NUMBER_VALUES = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    public final String[] ROMAN_NUMERAL_LETTERS = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
 }
